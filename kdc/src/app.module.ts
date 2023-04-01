ce5f8c3d700772c62888a9db5919c1af0f83d439
dalideco <dalissj1@gmail.com>
2023-04-01 17:20:19 +0100

    creating crypt and decrypt functions

--- kdc/src/app.module.ts
+++ kdc/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { KeyGenService } from './services/key-gen.service';
 
 @Module({
   imports: [
@@ -22,6 +23,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, KeyGenService],
 })
 export class AppModule {}
---
+++ kdc/src/services/key-gen.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import crypto from 'crypto';
+
+@Injectable()
+export class KeyGenService {
+  algorithm = 'aes-256-cbc';
+  initVector = crypto.randomBytes(16);
+  public generateUserSecretKey() {}
+
+  crypt(securityKey: string, message: string) {
+    const cipher = crypto.createCipheriv(
+      this.algorithm,
+      securityKey,
+      this.initVector,
+    );
+    let encryptedData = cipher.update(message, 'utf-8', 'hex');
+    encryptedData += cipher.final('hex');
+    return encryptedData;
+  }
+
+  decrypt(securityKey: string, encryptedData: string) {
+    const decipher = crypto.createDecipheriv(
+      this.algorithm,
+      securityKey,
+      this.initVector,
+    );
+    let decryptedData = decipher.update(encryptedData, 'hex', 'utf-8');
+    decryptedData += decipher.final('utf8');
+    return decryptedData;
+  }
+}
