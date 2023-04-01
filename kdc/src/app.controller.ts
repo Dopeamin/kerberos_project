64ae3b9ddfdba0b42135d9292f07ef4bf5ce4ce8
dalideco <dalissj1@gmail.com>
2023-04-01 21:42:47 +0100

    creating singup route

--- kdc/src/app.controller.ts
+++ kdc/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('signup')
+  postSignup(@Body() body) {
+    const { username, password } = body;
+    return this.appService.signup(username, password);
   }
 }
--- kdc/src/app.service.ts
+++ kdc/src/app.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { KeyGenService } from './services/key-gen.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(User)
+    private users: Repository<User>,
+    private keygen: KeyGenService,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async signup(username: string, password: string) {
+    const similarUser = await this.users.findOneBy({ username });
+    if (similarUser) {
+      console.log(similarUser);
+      throw new HttpException('Username already exists', HttpStatus.CONFLICT);
+    }
+
+    const secret_key = this.keygen.generateUserSecretKey(password, username);
+    const user = this.users.create({ username, secret_key });
+    this.users.save(user);
+    return user;
   }
 }
