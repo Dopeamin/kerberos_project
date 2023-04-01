5148c33801dde5b78ea9c0c81999f73ad554efd9
Mohamed Amine Hamdouni <mohamed@mvst.co>
2023-04-01 15:49:33 +0100

    finishes signup page

---
+++ frontend/src/components/shared/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import * as React from "react";
+
+export interface IHeaderProps {}
+
+export default function Header(props: IHeaderProps) {
+  return (
+    <div className="w-full flex justify-center align-center">
+      <div className="w-full flex max-w-7xl p-10 justify-between items-center gap-20">
+        <h1 className="text-3xl tracking-widest text-gray-100 font-bold">
+          Insatiable
+        </h1>
+
+        <div className="flex justify-evenly text-gray-200 gap-10 text-sm font-semibold items-center">
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,73 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import * as React from "react";
+
+export interface ISignUpProps {}
+
+export default function SignUp(props: ISignUpProps) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const router = useRouter();
+  const onClick = async (e: any) => {};
+
+  const setEmailValue = (e: any) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
+
+  const setPasswordValue = (e: any) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <div className="flex justify-center w-full items-center">
+      <div className="flex flex-col w-full max-w-7xl mt-10 items-center">
+        <h1 className="text-center text-white text-4xl font-bold">
+          Register your account
+        </h1>
+        <p className="px-4 max-w-4xl text-center text-sm text-gray-400 py-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          deleniti nisi magnam dignissimos consequatur eligendi ipsam veniam
+          voluptate nobis sint expedita ratione at, suscipit rem qui aut sed.
+          Fugit, itaque.
+        </p>
+        <div className="py-10 px-4 w-full flex justify-center">
+          <form className="flex flex-col gap-4 w-full max-w-[600px]">
+            <div>
+              <input
+                className="py-4 px-6 w-full bg-zinc-800 rounded text-zinc-300 outline-none focus:shadow-xl transition-all ease-in-out"
+                placeholder="Email"
+                type="email"
+                name="email"
+                onKeyUp={setEmailValue}
+              ></input>
+            </div>
+            <div className="flex flex-col md:flex-row gap-4 justify-center">
+              <input
+                className="py-4 px-6 flex-1 bg-zinc-800 rounded text-zinc-300 outline-none focus:shadow-xl transition-all ease-in-out"
+                placeholder="Password"
+                onKeyUp={setPasswordValue}
+                type="password"
+                name="password"
+              ></input>
+              <input
+                className="py-4 px-6 flex-1 bg-zinc-800 rounded text-zinc-300 outline-none focus:shadow-xl transition-all ease-in-out"
+                placeholder="Confirm Password"
+                type="password"
+                name="confirmPassword"
+              ></input>
+            </div>
+            <button
+              className="w-full bg-purple-600 py-4 mt-10 px-4 rounded-md text-white font-semibold text-lg transition-all ease-out hover:-translate-y-1 hover:shadow-lg disabled:opacity-20"
+              onClick={onClick}
+              disabled={isLoading}
+            >
+              <p>Sign Up</p>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
