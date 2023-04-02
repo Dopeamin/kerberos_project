bd743a94a75e9f15195fcf4639826895d8cf4ed0
dalideco <dalissj1@gmail.com>
2023-04-02 17:56:41 +0100

    decrypting user authenticator

--- chatgpt-server/src/app.service.ts
+++ chatgpt-server/src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { decrypt } from './utils/crypto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(encUserAuthenticator: string, encServiceTicket: string): string {
+    console.log(encUserAuthenticator, encServiceTicket);
+    console.log(process.env.SERVICE_SECRET);
+
+    // decrypting service ticket
+    const ServiceTicketStr = decrypt(
+      process.env.SERVICE_SECRET,
+      encServiceTicket,
+    );
+
+    if (!ServiceTicketStr)
+      throw new HttpException(
+        'Could not decrypt the sercit ticket',
+        HttpStatus.UNAUTHORIZED,
+      );
+
+    const serviceTicket = JSON.parse(ServiceTicketStr);
+
+    return serviceTicket;
   }
 }
