0d939ce3f5beaccab6b6628674d7d65d140d6c50
dalideco <dalissj1@gmail.com>
2023-04-02 15:14:37 +0100

    adding crypto functions to frontend

---
+++ frontend/src/utils/crypto.ts
@@ -0,0 +1,19 @@
+import { createHash } from "crypto";
+import { AES, enc } from "crypto-js";
+
+export function generateUserSecretKey(
+  password: string,
+  username: string,
+  version = "1"
+) {
+  const stringToHash = password + username + version;
+  return createHash("sha256").update(stringToHash).digest("hex");
+}
+
+export function crypt(securityKey: string, message: string) {
+  return AES.encrypt(message, securityKey).toString();
+}
+
+export function decrypt(securityKey: string, encryptedData: string) {
+  return AES.decrypt(encryptedData, securityKey).toString(enc.Utf8);
+}
